25a9ee7d0d7abb26ac89ef0fa4e0626370911e99
ChristopherMaurer <142697362+ChristopherMaurer@users.noreply.github.com>
2023-10-03 15:32:40 -0400

    Parallel threads for Excel Index Build
    Execute primary key index builds in multiple parallel threads (one per worksheet).  Get ready for the day when threads can actually run at the same time.

--- ExcelWriter.py
+++ ExcelWriter.py
@@ -3,6 +3,8 @@ from openpyxl import load_workbook
 import OFXWriter
 import re
 from os import path
+import threading
+
 
 class ConfigurationError(Exception):
     pass
@@ -10,11 +12,42 @@ class ConfigurationError(Exception):
 
 # Searching for duplicate primary keys is slow.  This attempts to speed it up by putting all primary keys into a
 # dictionary structure, thus effectively making a hash index out of the primary key.  The dictionary key is the primary
-# key of the EXCEL record and the value is the row number of the entry in the sheet, making it a direct access to the
-# matching record.  The indexing is performed in Excel's initializer block with the idea of turning it into a parallel
-# thread/process some day when true parallelism is possible in Python in frozen .EXEs (created with PYInstaller).
-# This would let the indexing take place at the same time the main program was accumulating its data records before
-# writing them out.
+# key of the EXCEL record and the item is the row number of the entry in the sheet, making it a direct access to the
+# matching record.  The indexing is performed in a parallel thread, thus letting (in theory) the indexing take place at
+# the same time the main program is accumulating its data records before writing them out.  Note that multithreading
+# does not imply parallel processing in Python 3.11.  We'll need a future release of Python before two totally
+# compute-bound threads can execute in parallel and we can reap some time savings.
+def IndexOneWorksheet(tablename, ws, MapSpecs):
+    pvt = threading.local()
+    for pvt.colhdrs in ws.iter_rows(min_col=1, max_col=ws.max_column, min_row=1, max_row=1):
+        break
+    ws.colnbrs = []
+    for pvt.OFXcol in MapSpecs.Cols:
+        for pvt.wscol in pvt.colhdrs:
+            if pvt.OFXcol == pvt.wscol.value:
+                ws.colnbrs.append(pvt.wscol.col_idx)
+                break
+        else:
+            raise ConfigurationError(
+                "Column {0} is not in spreadsheet for Table {1}".format(pvt.OFXcol, tablename))
+    pvt.pkcolnbrs = []
+    for pvt.PKItem in MapSpecs.PKCols:
+        for pvt.wscol in pvt.colhdrs:
+            if pvt.PKItem == pvt.wscol.value:
+                pvt.pkcolnbrs.append(pvt.wscol.col_idx - 1)
+                break
+        else:
+            raise ConfigurationError(
+                "PK Column {0} is not in spreadsheet for Table {1}".format(pvt.PKItem, tablename))
+    ws.PKIndex = {}
+    pvt.rownum = 2
+    for pvt.row in ws.iter_rows(min_col=1, max_col=ws.max_column, min_row=pvt.rownum, max_row=ws.max_row):
+        pvt.PK = tuple(pvt.row[i].value for i in pvt.pkcolnbrs)
+        ws.PKIndex[pvt.PK] = pvt.rownum
+        pvt.rownum += 1
+    ws.PKIndexIsReady = True
+
+
 def IndexAllWorksheets(MapSpecs, wb):
     for OFX in MapSpecs:
         for tablename in MapSpecs[OFX]:
@@ -23,34 +56,9 @@ def IndexAllWorksheets(MapSpecs, wb):
                 if (not hasattr(ws,"PKIndexIsReady")) and ws.max_row>1:
                     ws.PKIndexIsReady = False
                     spectuple = MapSpecs[OFX][tablename][0]
-                    for colhdrs in ws.iter_rows(min_col=1, max_col=ws.max_column, min_row=1, max_row=1):
-                        break
-                    ws.colnbrs = []
-                    for OFXcol in spectuple.Cols:
-                        for wscol in colhdrs:
-                            if OFXcol == wscol.value:
-                                ws.colnbrs.append(wscol.col_idx)
-                                break
-                        else:
-                            raise ConfigurationError(
-                                "Column {0} is not in spreadsheet for Table {1}".format(OFXcol, tablename))
-                    pkcolnbrs = []
-                    for PKItem in spectuple.PKCols:
-                        for wscol in colhdrs:
-                            if PKItem == wscol.value:
-                                pkcolnbrs.append(wscol.col_idx-1)
-                                break
-                        else:
-                            raise ConfigurationError(
-                                "Column {0} is not in spreadsheet for Table {1}".format(PKItem, tablename))
-                    ws.PKIndex = {}
-                    rownum = 2
-                    for row in ws.iter_rows(min_col=1, max_col=ws.max_column, min_row=rownum, max_row=ws.max_row):
-                        PK = tuple(row[i].value for i in pkcolnbrs)
-                        ws.PKIndex[PK] = rownum
-                        rownum += 1
-                    ws.PKIndexIsReady = True
-
+#                    IndexOneWorksheet(tablename, ws, spectuple)  # Old invocation - called as a function, not as a thread
+                    ws.IndexThread = threading.Thread(target=IndexOneWorksheet, args=(tablename, ws, spectuple))
+                    ws.IndexThread.start()  # Start up each worksheet's index build in a separate thread
 
 
 class ExcelWBWriter(OFXWriter.Writer):
@@ -86,12 +94,15 @@ class ExcelWBWriter(OFXWriter.Writer):
                     if EachTable not in self.Stats:
                         self.Stats[EachTable] = [0, 0]  # Get ready to update statistics
                     origmaxrow = ws.max_row
+                    if hasattr(ws,"IndexThread"): ws.IndexThread.join()   # synchronize here to the parallel thread indexing this worksheet.
+                    if not ws.PKIndexIsReady:   # Thread ended, but successfully or failure?
+                        raise ConfigurationError('Failed to Create Unique Index')
                     for datarow, thisPK in zip(self.curOFXList[EachTable][2], self.curOFXList[EachTable][3]):
                         self.__anychanges = True
                         destrow = ws.PKIndex[thisPK] if thisPK in ws.PKIndex else ws.max_row+1
                         for newvalue, destcol in zip(datarow,ws.colnbrs):
                             ws.cell(row=destrow, column=destcol).value = newvalue
-                        if destrow>origmaxrow:
+                        if destrow > origmaxrow:
                             self.Stats[EachTable][0] += 1
                         else:
                             self.Stats[EachTable][1] += 1
